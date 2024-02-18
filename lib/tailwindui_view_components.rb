3fd7bf8760aa5a698fa8b581b86e06a3251ab732
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-02-17 22:05:18 -0500

    Initial commit

---
+++ lib/tailwindui_view_components.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tailwindui_view_components/version"
+
+module TailwinduiViewComponents
+  class Error < StandardError; end
+  # Your code goes here...
+end
